e08cbbf97b6ce9c69d1808c933e63964d64ec239
Konstantin F <kf4654569@gmail.com>
2025-01-01 23:25:39 +0300

    feat: add extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+  const string = String(str);
+  const addition = options.addition !== undefined ? options.addition : '';
+  const addSeparator = options.additionSeparator ? options.additionSeparator : '|';
+  const separator = options.separator ? options.separator : '+';
+  const aTimes = options.additionRepeatTimes ? options.additionRepeatTimes : null;
+  const times = options.repeatTimes ? options.repeatTimes : null;
+  let result = '';
+  if (!times && !aTimes) {
+    return `${string}${addition}`;
+  }
+  if (!aTimes) {
+    for (let i = 0; i < times; i += 1) {
+      result += `${string}${String(addition)}${separator}`;
+  }
+  } else {
+    let part = string;
+    for (let i = 0; i < aTimes; i += 1) {
+      part += `${String(addition)}${addSeparator}`;
+    }
+    part = part.slice(0 , part.lastIndexOf(addSeparator))
+    for (let i = 0; i < times; i += 1) {
+      result += `${part}${separator}`
+    }
+  }
+
+  return result.slice(0, result.lastIndexOf(separator));
 }
 
 module.exports = {
